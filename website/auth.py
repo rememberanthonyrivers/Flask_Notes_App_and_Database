dc0db33f25edb6466d7763a67e83d96c628ea6e6
tony <hireriversanthony@gmail.com>
2022-07-09 11:37:33 -0400

    Added new forms for the login and signup page as well as adding https handling

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-# stores all url endpoints (front end)
+# stores all url endpoints for the front end
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
@@ -12,6 +12,26 @@ def login():
 def logout():
     return '<p>Logout</p>'
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':    #   if the request is a post request...
+        email = request.form.get('email')   #   now lets grab our form data and assign them to a var
+        firstname = request.form.get('firstName')   #   now lets assign our form data to a var
+        password1 = request.form.get('password1')   #   now lets assign our form data to a var
+        password2 = request.form.get('password2')   #   now lets assign our form data to a var
+
+        #   below lets use control flow to figure out what to do with the user
+        if len(email) < 4:
+            pass
+        elif len(firstname) < 2:
+            pass
+        elif password1 != password2:
+            pass 
+        elif len(password1) < 7:
+            pass
+        else: 
+            #add user to database 
+            pass
+
+
     return render_template('sign_up.html')
